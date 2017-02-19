33037fa2f079fbe2013aeaae68ed4c384f0819bb
Richard Drizin <github@drizin.io>
2017-02-19 04:21:00 -0300

    New custom entities, new custom properties/relationships.

---
+++ ExtensibleAdventureWorks.Business/CustomEntities/OpeningHour.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensibleAdventureWorks.Business.Entities
+{
+    #region Module Setup
+    public class OpeningHourModule : IModelExtension
+    {
+        public void Extend(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new custom_OpeningHourConfiguration());
+        }
+    }
+    #endregion
+
+    #region New POCO - OpeningHour
+    // OpeningHour
+    [System.CodeDom.Compiler.GeneratedCode("EF.Reverse.POCO.Generator", "2.28.0.0")]
+    public partial class custom_OpeningHour
+    {
+        public int BusinessEntityId { get; set; } // BusinessEntityID (Primary key)
+        public int DayOfWeek { get; set; } // DayOfWeek (Primary key)
+        public System.TimeSpan OpeningTime { get; set; } // OpeningTime
+        public System.TimeSpan ClosingTime { get; set; } // ClosingTime
+
+        // Foreign keys
+
+        /// <summary>
+        /// Parent Sales_Store pointed by [OpeningHour].([BusinessEntityId]) (FK_OpeningHour_Store_BusinessEntityID)
+        /// </summary>
+        public virtual Sales_Store Sales_Store { get; set; } // FK_OpeningHour_Store_BusinessEntityID
+    }
+    #endregion
+
+    #region OpeningHour Configuration
+    [System.CodeDom.Compiler.GeneratedCode("EF.Reverse.POCO.Generator", "2.28.0.0")]
+    public partial class custom_OpeningHourConfiguration : System.Data.Entity.ModelConfiguration.EntityTypeConfiguration<custom_OpeningHour>
+    {
+        public custom_OpeningHourConfiguration()
+            : this("custom")
+        {
+        }
+
+        public custom_OpeningHourConfiguration(string schema)
+        {
+            ToTable("OpeningHour", schema);
+            HasKey(x => new { x.BusinessEntityId, x.DayOfWeek });
+
+            Property(x => x.BusinessEntityId).HasColumnName(@"BusinessEntityID").HasColumnType("int").IsRequired().HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None);
+            Property(x => x.DayOfWeek).HasColumnName(@"DayOfWeek").HasColumnType("int").IsRequired().HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None);
+            Property(x => x.OpeningTime).HasColumnName(@"OpeningTime").HasColumnType("time").IsRequired();
+            Property(x => x.ClosingTime).HasColumnName(@"ClosingTime").HasColumnType("time").IsRequired();
+
+            // Foreign keys
+            HasRequired(a => a.Sales_Store).WithMany(b => b.custom_OpeningHours).HasForeignKey(c => c.BusinessEntityId).WillCascadeOnDelete(false); // FK_OpeningHour_Store_BusinessEntityID
+            this.ConfigureExtensions();
+            InitializePartial();
+        }
+        partial void InitializePartial();
+    }
+    #endregion
+
+    #region Partial Classes
+    partial class AdventureWorksDB
+    {
+        public System.Data.Entity.DbSet<custom_OpeningHour> custom_OpeningHours { get; set; } // OpeningHour
+    }
+    #endregion
+
+}
---
+++ ExtensibleAdventureWorks.Business/CustomEntities/TaxAuditor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensibleAdventureWorks.Business.Entities
+{
+    #region Module Setup
+    public class TaxAuditorModule : IModelExtension, IModelExtension<Sales_Store>
+    {
+        public void Extend(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new custom_TaxAuditorConfiguration());
+        }
+        public void Extend(EntityTypeConfiguration<Sales_Store> config)
+        {
+            config.Property(x => x.TaxAuditorId).HasColumnName(@"TaxAuditorID").HasColumnType("int").IsOptional();
+            config.HasOptional(a => a.custom_TaxAuditor).WithMany(b => b.Sales_Stores).HasForeignKey(c => c.TaxAuditorId).WillCascadeOnDelete(false); // FK_Store_TaxAuditor_TaxAuditorID
+        }
+    }
+    #endregion
+
+    #region New POCO - TaxAuditor
+    // TaxAuditor
+    [System.CodeDom.Compiler.GeneratedCode("EF.Reverse.POCO.Generator", "2.28.0.0")]
+    public partial class custom_TaxAuditor
+    {
+        public int TaxAuditorId { get; set; } // TaxAuditorID (Primary key)
+        public string FirstName { get; set; } // FirstName (length: 200)
+        public string LastName { get; set; } // LastName (length: 200)
+
+        // Reverse navigation
+
+        /// <summary>
+        /// Child Sales_Stores where [Store].[TaxAuditorID] point to this entity (FK_Store_TaxAuditor_TaxAuditorID)
+        /// </summary>
+        public virtual System.Collections.Generic.ICollection<Sales_Store> Sales_Stores { get; set; } = new System.Collections.Generic.List<Sales_Store>(); // Store.FK_Store_TaxAuditor_TaxAuditorID
+    }
+    #endregion
+
+    #region TaxAuditor Configuration
+    [System.CodeDom.Compiler.GeneratedCode("EF.Reverse.POCO.Generator", "2.28.0.0")]
+    public partial class custom_TaxAuditorConfiguration : System.Data.Entity.ModelConfiguration.EntityTypeConfiguration<custom_TaxAuditor>
+    {
+        public custom_TaxAuditorConfiguration()
+            : this("custom")
+        {
+        }
+
+        public custom_TaxAuditorConfiguration(string schema)
+        {
+            ToTable("TaxAuditor", schema);
+            HasKey(x => x.TaxAuditorId);
+
+            Property(x => x.TaxAuditorId).HasColumnName(@"TaxAuditorID").HasColumnType("int").IsRequired().HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+            Property(x => x.FirstName).HasColumnName(@"FirstName").HasColumnType("nvarchar").IsOptional().HasMaxLength(200);
+            Property(x => x.LastName).HasColumnName(@"LastName").HasColumnType("nvarchar").IsOptional().HasMaxLength(200);
+            this.ConfigureExtensions();
+            InitializePartial();
+        }
+        partial void InitializePartial();
+    }
+    #endregion
+
+    #region Partial Classes
+    partial class AdventureWorksDB
+    {
+        public System.Data.Entity.DbSet<custom_TaxAuditor> custom_TaxAuditors { get; set; } // TaxAuditor
+    }
+    partial class Sales_Store
+    {
+        public int? TaxAuditorId { get; set; } // TaxAuditorID
+
+        /// <summary>
+        /// Child custom_OpeningHours where [OpeningHour].[BusinessEntityID] point to this entity (FK_OpeningHour_Store_BusinessEntityID)
+        /// </summary>
+        public virtual System.Collections.Generic.ICollection<custom_OpeningHour> custom_OpeningHours { get; set; } = new System.Collections.Generic.List<custom_OpeningHour>(); // OpeningHour.FK_OpeningHour_Store_BusinessEntityID
+
+        /// <summary>
+        /// Parent custom_TaxAuditor pointed by [Store].([TaxAuditorId]) (FK_Store_TaxAuditor_TaxAuditorID)
+        /// </summary>
+        public virtual custom_TaxAuditor custom_TaxAuditor { get; set; } // FK_Store_TaxAuditor_TaxAuditorID
+
+    }
+    #endregion
+}
