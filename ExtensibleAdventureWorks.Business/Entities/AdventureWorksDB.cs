5d8d2ac9f6f6d913b9dd981697a87020fad2ab07
Richard Drizin <github@drizin.io>
2017-02-19 00:00:37 -0300

    T4 template generating AdventureWorks2008 Added EntityFramework (WithHierarchyId), Dapper, nUnit, and Tests Project.

---
+++ ExtensibleAdventureWorks.Business/Entities/AdventureWorksDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensibleAdventureWorks.Business.Entities
+{
+    partial class AdventureWorksDB
+    {
+        private static void AdventureWorksDBStaticPartial()
+        {
+        }
+    }
+}
---
+++ ExtensibleAdventureWorks.Tests/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensibleAdventureWorks.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var db = new ExtensibleAdventureWorks.Business.Entities.AdventureWorksDB();
+            var stores = db.Sales_Stores.ToList();
+            Assert.That(stores.Count > 1);
+        }
+    }
+}
